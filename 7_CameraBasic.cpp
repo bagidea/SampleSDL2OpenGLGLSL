cb928d92033510b9d2d0e41c6bc660a5ebad464c
bagidea <aodbboy@hotmail.com>
2015-07-03 04:01:43 +0700

    Upload Sample CameraBasic

---
+++ 7_CameraBasic.cpp
@@ -0,0 +1,341 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <ctime>
+
+#include <GL/glew.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+using namespace std;
+
+SDL_Window* window;
+SDL_GLContext gl;
+
+GLuint program;
+GLuint vertexShader;
+GLuint fragmentShader;
+
+GLuint VAO, VBO;
+
+GLuint gTransformation;
+
+GLuint gTexture1;
+
+GLfloat num;
+bool chk;
+
+unsigned int tex;
+
+glm::vec3 posBox[10];
+
+unsigned int LoadImage(string path)
+{
+	SDL_Surface* img = IMG_Load(path.c_str());
+	unsigned int id;
+
+	if(img == NULL)
+	{
+		cout << "Can't Load Image : " << path << endl;
+	}else{
+		glGenTextures(1, &id);
+		glBindTexture(GL_TEXTURE_2D, id);
+
+		GLenum formatTexture;
+		switch(img->format->BytesPerPixel)
+		{
+		case 4:
+			if(img->format->Rmask == 0x000000ff)
+			{
+				formatTexture = GL_RGBA;
+			}else{
+				formatTexture = GL_BGRA;
+			}
+			break;
+		case 3:
+			if(img->format->Rmask == 0x000000ff)
+			{
+				formatTexture = GL_RGB;
+			}else{
+				formatTexture = GL_BGR;
+			}
+			break;
+		default:
+			cout << "The Image not Truecolor." << endl;
+		}
+
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+		glTexImage2D(GL_TEXTURE_2D, 0, formatTexture, img->w, img->h, 0, formatTexture, GL_UNSIGNED_BYTE, img->pixels);
+
+		SDL_FreeSurface(img);
+		img = NULL;
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+
+	return id;
+}
+
+string LoadFile(string path)
+{
+	string line, output;
+	output = "";
+
+	ifstream in(path.c_str());
+	if(!in.is_open())
+	{
+		cout << "Can't Load File " << path << endl;
+		return output;
+	}else{
+		while(in.good())
+		{
+			getline(in, line);
+			output.append(line+"\n");
+		}
+	}
+
+	return output;
+}
+
+GLuint CompileShader(string text, unsigned int shaderType)
+{
+	GLuint shader = glCreateShader(shaderType);
+
+	const char* source[1];
+	source[0] = text.c_str();
+
+	GLint length[1];
+	length[0] = text.length();
+
+	glShaderSource(shader, 1, source, length);
+	glCompileShader(shader);
+
+	GLint success;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	if(!success)
+	{
+		GLchar error[1024];
+		glGetShaderInfoLog(shader, 1024, NULL, error);
+		cout << error << endl;
+	}
+
+	return shader;
+}
+
+void LoadShader(string _vertexShader, string _fragmentShader)
+{
+	program = glCreateProgram();
+	vertexShader = CompileShader(LoadFile(_vertexShader), GL_VERTEX_SHADER);
+	fragmentShader = CompileShader(LoadFile(_fragmentShader), GL_FRAGMENT_SHADER);
+
+	if(vertexShader == NULL && fragmentShader == NULL)
+	{
+		cout << "Compile Shader Fail." << endl;
+	}else{
+		glAttachShader(program, vertexShader);
+		glAttachShader(program, fragmentShader);
+		glLinkProgram(program);
+
+		GLint success;
+		glGetProgramiv(program, GL_LINK_STATUS, &success);
+		if(!success)
+		{
+			GLchar error[1024];
+			glGetProgramInfoLog(program, 1024, NULL, error);
+			cout << error << endl;	
+		}
+	}
+}
+
+void Init()
+{
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+	window = SDL_CreateWindow("Camera Basic", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
+	
+	IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG);
+
+	gl = SDL_GL_CreateContext(window);
+	glewInit();
+
+	glEnable(GL_DEPTH_TEST);
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+	LoadShader("source/shader/Shader4.vs", "source/shader/Shader4.fs");
+}
+
+void Start()
+{
+	gTransformation = glGetUniformLocation(program, "gWorld");
+	
+	gTexture1 = glGetUniformLocation(program, "outTexture1");
+
+	num = 0.0f;
+	chk = false;
+
+	tex = LoadImage("source/box.png");
+
+	posBox[0] = glm::vec3( 0.0f,  0.0f,  0.0f);
+  	posBox[1] = glm::vec3( 2.0f,  5.0f, -15.0f); 
+  	posBox[2] = glm::vec3(-1.5f, -2.2f, -2.5f); 
+	posBox[3] = glm::vec3(-3.8f, -2.0f, -12.3f);  
+	posBox[4] = glm::vec3( 2.4f, -0.4f, -3.5f);  
+	posBox[5] = glm::vec3(-1.7f,  3.0f, -7.5f);  
+	posBox[6] = glm::vec3( 1.3f, -2.0f, -2.5f);  
+	posBox[7] = glm::vec3( 1.5f,  2.0f, -2.5f); 
+	posBox[8] = glm::vec3( 1.5f,  0.2f, -1.5f); 
+	posBox[9] = glm::vec3(-1.3f,  1.0f, -1.5f);
+
+	GLfloat vec[] = {
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+	     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+	     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+	    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+	    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+	    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+	     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+	    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+	    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+	};
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vec), vec, GL_STATIC_DRAW);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+		glEnableVertexAttribArray(1);
+	glBindVertexArray(0);
+}
+
+void Update()
+{
+	num += 0.001f;
+
+	GLfloat camX = sinf(SDL_GetTicks() * 0.001f) * 5.0f;
+	GLfloat camY = cosf(SDL_GetTicks() * 0.001f) * 5.0f;
+
+	glm::mat4 projection;
+	glm::mat4 view;
+
+	projection = glm::perspective(45.0f, 800.0f/600.0f, 0.1f, 100.0f);
+	view = glm::lookAt(glm::vec3(camX, 0.0f, camY), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+	glUseProgram(program);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, tex);
+	glUniform1i(gTexture1, 0);
+
+	glBindVertexArray(VAO);
+		for(int i = 0; i < 10; i++)
+		{
+			glm::mat4 model;
+			model = glm::translate(model, posBox[i]);
+			model = glm::rotate(model, 20.0f * i, glm::vec3(1.0f, 0.3f, 0.5f));
+
+			glUniformMatrix4fv(gTransformation, 1, GL_FALSE, glm::value_ptr(projection * view * model));
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
+	glBindVertexArray(0);
+}
+
+void Close()
+{
+	glDetachShader(program, vertexShader);
+	glDeleteShader(vertexShader);
+	glDetachShader(program, fragmentShader);
+	glDeleteShader(fragmentShader);
+	glDeleteProgram(program);
+
+	SDL_GL_DeleteContext(gl);
+	SDL_DestroyWindow(window);
+	window = NULL;
+}
+
+int main(int argc, char* argv[])
+{
+	Init();
+	Start();
+
+	bool quit = false;
+	SDL_Event e;
+
+	while(!quit)
+	{
+		while(SDL_PollEvent(&e) != 0)
+		{
+			if(e.type == SDL_QUIT)
+			{
+				quit = true;
+				break;
+			}
+			else if(e.type == SDL_KEYDOWN)
+			{
+				if(e.key.keysym.sym == SDLK_ESCAPE)
+				{
+					quit = true;
+					break;
+				}
+			}
+		}
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		Update();
+		SDL_GL_SwapWindow(window);
+	}
+
+	Close();
+
+	return 0;
+}
